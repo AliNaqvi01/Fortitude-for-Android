d9fc2f19849e724e7ea2a1b3ef57731bfbe4cb74
Ali <Alinaqvi01n@gmail.com>
2018-04-07 20:26:39 -0400

    Initial

--- app/src/main/java/com/fume/ali/getrequests/Main2Activity.java
+++ app/src/main/java/com/fume/ali/getrequests/Main2Activity.java
@@ -11,6 +11,9 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.animation.OvershootInterpolator;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.florent37.awesomebar.ActionItem;
@@ -18,6 +21,7 @@ import com.github.florent37.awesomebar.AwesomeBar;
 import com.robinhood.ticker.TickerUtils;
 import com.robinhood.ticker.TickerView;
 import com.shashank.sony.fancytoastlib.FancyToast;
+import com.yarolegovich.lovelydialog.LovelyInfoDialog;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -25,39 +29,35 @@ import java.util.Arrays;
 
 public class Main2Activity extends AppCompatActivity {
 
+    ListView mListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-
-        final TickerView tickerView = findViewById(R.id.tickerView);
-        tickerView.setCharacterList(TickerUtils.getDefaultNumberList());
-        final TickerView tickerView2 = findViewById(R.id.tickerView2);
-        tickerView2.setCharacterList(TickerUtils.getDefaultNumberList());
-        final TickerView tickerView3 = findViewById(R.id.tickerView3);
-        tickerView3.setCharacterList(TickerUtils.getDefaultNumberList());
-        final TickerView tickerView4 = findViewById(R.id.tickerView4);
-        tickerView4.setCharacterList(TickerUtils.getDefaultNumberList());
-        final TickerView tickerView5 = findViewById(R.id.tickerView5);
-        tickerView5.setCharacterList(TickerUtils.getDefaultNumberList());
-        final TickerView tickerView6 = findViewById(R.id.tickerView6);
-        tickerView6.setCharacterList(TickerUtils.getDefaultNumberList());
-
-        AwesomeBar bar3 = findViewById(R.id.bar3);
-        bar3.displayHomeAsUpEnabled(true);
-        bar3.addAction(R.drawable.awsb_ic_three_dots,"Details");
+        setContentView(R.layout.listview_layout);
+        mListView = findViewById(R.id.lister);
+        mListView.setDivider(null);
+        mListView.setDividerHeight(0);
+        ArrayList<Card> list = new ArrayList<>();
+        Intent intent = getIntent();
+        Log.e("Datra", Arrays.toString(intent.getStringArrayExtra("Info")));
+        AwesomeBar bar = findViewById(R.id.bar2);
+        bar.addAction(R.drawable.awsb_ic_edit_animated,"Details");
+        bar.displayHomeAsUpEnabled(true);
 
 
 
 
 
 
-        bar3.setOnMenuClickedListener(new View.OnClickListener() {
+
+
+        bar.setOnMenuClickedListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                onBackPressed();
 
+onBackPressed();
 
 
             }
@@ -65,66 +65,56 @@ public class Main2Activity extends AppCompatActivity {
 
 
 
-
-
-        Intent intent = getIntent();
-        Log.e("Datra", Arrays.toString(intent.getStringArrayExtra("Info")));
-
         String[] data = intent.getStringArrayExtra("Info");
-           Log.e("Data", (String) data[2]);
+        Log.e("Data", (String) data[2]);
         Log.e("Data", (String) data[1]);
         Log.e("Data", (String) data[0]);
+        list.add(new Card("drawable://" + R.drawable.victoryblurred, "Wins: " + String.valueOf(data[6])));
+        list.add(new Card("drawable://" + R.drawable.mapblurred, "Matches Played: " + String.valueOf(data[4])));
+        list.add(new Card("drawable://" + R.drawable.maptwoblurred, "K/D: " + data[5]));
+        list.add(new Card("drawable://" + R.drawable.map, "Yosemite"));
+        list.add(new Card("drawable://" + R.drawable.map, "Yosemite"));
+        list.add(new Card("drawable://" + R.drawable.map, "Yosemite"));
+        list.add(new Card("drawable://" + R.drawable.map, "Yosemite"));
+        CustomListAdapter adapter = new CustomListAdapter(this, R.layout.activity_main2, list);
+        mListView.setAdapter(adapter);
+
+
+
+        ListView listView = findViewById(R.id.listy);
+
+
+
+
+
+
+
+
+
+
 
         final String[] info = intent.getStringArrayExtra("Indiviudals");
-
-
-
-
-        tickerView.setAnimationDuration(2000);
-        tickerView.setAnimationInterpolator(new OvershootInterpolator());
-        tickerView.setGravity(Gravity.CENTER);
-        tickerView2.setAnimationDuration(2000);
-        tickerView2.setAnimationInterpolator(new OvershootInterpolator());
-        tickerView2.setGravity(Gravity.CENTER);
-        tickerView3.setAnimationDuration(2000);
-        tickerView3.setAnimationInterpolator(new OvershootInterpolator());
-        tickerView3.setGravity(Gravity.CENTER);
-        tickerView4.setAnimationDuration(2000);
-        tickerView4.setAnimationInterpolator(new OvershootInterpolator());
-        tickerView4.setGravity(Gravity.CENTER);
-        tickerView5.setAnimationDuration(2000);
-        tickerView5.setAnimationInterpolator(new OvershootInterpolator());
-        tickerView5.setGravity(Gravity.CENTER);
-        tickerView6.setAnimationInterpolator(new OvershootInterpolator());
-        tickerView5.setAnimationDuration(4000);
-        tickerView6.setGravity(Gravity.CENTER);
-
-        tickerView.setText("AEC038%#8O");
-        tickerView.setText("Device: " + data[0]);
-        tickerView2.setText("AEC038%#8OAEC038%#8O");
-        tickerView2.setText("Total Score: " + data[1]);
-        tickerView3.setText("AEC038%#8OAEC038%#8O");
-        tickerView3.setText("Average match time: " + data[2]);
-        tickerView4 .setText("AEC038%#8OAEC038%#8O");
-        tickerView4.setText("Total play time: " + data[3]);
-        tickerView5 .setText("AEC038%#8OAEC038%#8O");
-        tickerView5 .setText("nabiuoog*&t&d&s*dG78");
-        tickerView5.setText("Matches played: " + data[4]);
-        tickerView6 .setText("AEC038%#8OAEC038%#8O");
-        tickerView6.setText("Totals kills" + data[5]);
-
-
-
-
-        bar3.setActionItemClickListener(new AwesomeBar.ActionItemClickListener() {
+        bar.setActionItemClickListener(new AwesomeBar.ActionItemClickListener() {
             @Override
             public void onActionItemClicked(int position, ActionItem actionItem) {
+
                 Intent intent2 = new Intent(getApplicationContext(), Main5Activity.class);
                 intent2.putExtra("Values", info);
                 startActivity(intent2);
 
+
+
+
             }
         });
+
+
+
+
+
+
+
+
 
 
 
